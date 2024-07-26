3155e4e299d42c8ea29164eaf08a7f8b3bffd095
KIMCHANGHWAN <z7431671@gmail.com>
2024-07-26 10:08:47 +0900

    feat gameDTO test

--- DrawServer/spring/dataReceiver.js
+++ DrawServer/spring/dataReceiver.js
@@ -1,11 +1,45 @@
 // dataReceiver.js
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const {
+    PlayerDTO,
+    GameRoomStatus,
+    RoundResult,
+} = require("../model/gameDTO.js");
 
-router.post('/receive-data', (req, res) => {
+router.post("/receive-data", (req, res) => {
     const data = req.body;
-    console.log("Received data:", data);
-    res.send('Data received');
+    // console.log("Received data:", data);
+
+    const players = data.playerList.map(
+        (player) =>
+            new PlayerDTO(
+                player.playerId,
+                player.nickName,
+                player.profileImage,
+                player.vicCnt,
+                player.gameCnt,
+                player.userId
+            )
+    );
+    const gameRoom = { id: data.gameRoomId, title: data.gameRoomTitle };
+    const settings = {
+        turnTimeLimit: data.turnTimeLeft,
+        roundTimeLimit: data.roundTimeLimit,
+        version: data.gameVersion,
+    };
+    const subjects = data.subjects;
+
+    const gameRoomStatus = new GameRoomStatus(
+        players,
+        gameRoom,
+        settings,
+        subjects
+    );
+
+    console.log("Received GameRoomStatus data:", gameRoomStatus.toJSON());
+
+    res.send("Data received");
 });
 
 module.exports = router;
