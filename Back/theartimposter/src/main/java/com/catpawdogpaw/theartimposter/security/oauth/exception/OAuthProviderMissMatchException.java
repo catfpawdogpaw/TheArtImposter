d921945b66d7cedf04ca0a4951ec881a0411f4e5
jamw <jawoo9797>
2024-07-29 08:55:43 +0900

    git ignore설정 변경

--- Back/theartimposter/src/main/java/com/catpawdogpaw/theartimposter/security/oauth/exception/OAuthProviderMissMatchException.java
+++
@@ -1,7 +0,0 @@
-package com.catpawdogpaw.theartimposter.security.oauth.exception;
-
-public class OAuthProviderMissMatchException extends RuntimeException {
-    public OAuthProviderMissMatchException(String message) {
-        super(message);
-    }
-}
